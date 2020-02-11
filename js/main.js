658cfa88de03e9af7c7299bdbd1b4df198df4471
Kaputin <kaputinevgeny@gmail.com>
2020-02-11 05:02:52 +0300

    Промежуточное домашнее задание, вопросы прописал в комментариях

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ var photos = [];
 var arrayTotalNumbersUrl = [];
 var arrayRandomNumbersUrl = [];
 
+// сказали упростить данную функцию, задать массив и просто его перемешать, а затем из него брать подряд элементы? или как лучше реализовать, здесь делал - думал может еще где пригодится
 var getArrayRandomNumberUrl = function (min, max) {
   var tempRandomNumber;
   var totalNumbers = max - min + 1;
@@ -92,3 +93,186 @@ for (var i = 0; i < photos.length; i++) {
   fragment.appendChild(renderPhoto(photos[i]));
 }
 similarListElement.appendChild(fragment);
+
+// --------------------------------Добавление фото и открытие, закрытие формы--------------------------------------------- //
+
+var ESC_KEY = 'Escape';
+var ENTER_KEY = 'Enter';
+
+var body = document.querySelector('body');
+var uploadFile = document.querySelector('#upload-file');
+var imgUploadOverlay = document.querySelector('.img-upload__overlay');
+var uploadCancel = document.querySelector('#upload-cancel');
+var textDescription = document.querySelector('.text__description');
+var textHashtags = document.querySelector('.text__hashtags');
+
+var onPopupEscPress = function (evt) {
+  if (textDescription === document.activeElement || textHashtags === document.activeElement) {
+    return;
+  } else if (evt.key === ESC_KEY) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  if (uploadFile.files.length > 0) {
+    body.classList.add('modal-open');
+    imgUploadOverlay.classList.remove('hidden');
+    document.addEventListener('keydown', onPopupEscPress);
+  }
+};
+
+uploadFile.addEventListener('change', openPopup);
+
+uploadFile.addEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    openPopup();
+  }
+});
+
+var closePopup = function () {
+  body.classList.remove('modal-open');
+  imgUploadOverlay.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+  uploadFile.value = null;
+};
+
+uploadFile.removeEventListener('change', closePopup);
+
+uploadFile.removeEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    closePopup();
+  }
+});
+
+uploadCancel.addEventListener('click', function () {
+  closePopup();
+});
+
+uploadCancel.addEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    closePopup();
+  }
+});
+
+uploadCancel.removeEventListener('click', openPopup);
+
+uploadCancel.removeEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    openPopup();
+  }
+});
+
+// --------------Ползунок-------------------- //
+
+var effectList = document.querySelector('.effects__list');
+var previewImg = document.querySelector('.img-upload__preview img');
+
+var filterChangeHandler = function (evt) {
+  if (evt.target) {
+    previewImg.className = 'effects__preview--' + evt.target.value;
+  }
+};
+
+effectList.addEventListener('change', filterChangeHandler);
+
+// --------------Валидация-------------------- //
+var getFindRepeated = function (arrHashtags) {
+  arrHashtags.sort();
+  for (var j = 0; j < arrHashtags.length - 1; j++) {
+    if (arrHashtags[j] === arrHashtags[j + 1]) {
+      return true;
+    }
+  }
+  return false;
+};
+
+textHashtags.addEventListener('input', function (evt) {
+  var target = evt.target;
+  var hashtagsList = target.value.toLowerCase().split(' ');
+
+  // все равно пропускает если несколько хеш-тегов, буду переделывать, а так же еще нету проверок:
+  // строка после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т.п.), символы пунктуации (тире, дефис, запятая и т.п.), эмодзи и т.д.;
+
+  for (var h = 0; h < hashtagsList.length; h++) {
+    if (hashtagsList[h].charAt(0) !== '#') {
+      target.setCustomValidity(
+          'Хэш-тег должен начинаться с символа # (решётка)'
+      );
+    } else if (hashtagsList[h].length === 1 && hashtagsList[h].charAt(0) === '#') {
+      target.setCustomValidity(
+          'Хеш-тег не должен состоять только из одной решётки'
+      );
+    } else if (hashtagsList[h].length > 20) {
+      target.setCustomValidity(
+          'Максимальная длина одного хэш-тега не должна превышать 20 символов, включая решётку'
+      );
+    } else if (hashtagsList[h].split('#').length > 2) {
+      target.setCustomValidity(
+          'Хеш-теги должны разделяться пробелами'
+      );
+    } else if (getFindRepeated(hashtagsList)) {
+      target.setCustomValidity(
+          'Один и тот же хэш-тег не может быть использован дважды'
+      );
+    } else if (hashtagsList.length > 5) {
+      target.setCustomValidity(
+          'Нельзя указать больше пяти хэш-тегов'
+      );
+    } else {
+      target.setCustomValidity('');
+    }
+  }
+});
+
+
+// ползунок только потестил, чтобы двигался и с ним шкала заполнялась
+
+// var effectLevelPin = document.querySelector('.effect-level__pin');
+// var effectLevelDepth = document.querySelector('.effect-level__depth');
+// effectLevelPin.addEventListener('mousedown', function (evt) {
+//   evt.preventDefault();
+//   var startCoords = {
+//     x: evt.clientX,
+//   };
+//   var dragged = false;
+//   var onMouseMove = function (moveEvt) {
+//     moveEvt.preventDefault();
+//     dragged = true;
+//     var shift = {
+//       x: startCoords.x - moveEvt.clientX,
+//     };
+//     startCoords = {
+//       x: moveEvt.clientX,
+//     };
+//     var pzdc = (effectLevelPin.offsetLeft - shift.x);
+//     if (pzdc < 0) {
+//       effectLevelPin.style.left = 0 + 'px';
+//       effectLevelDepth.style.width = effectLevelPin.style.left;
+//     } else {
+//       if (pzdc > 450) {
+//         effectLevelPin.style.left = 450 + 'px';
+//         effectLevelDepth.style.width = effectLevelPin.style.left;
+//       } else {
+//         effectLevelPin.style.left = (effectLevelPin.offsetLeft - shift.x) + 'px';
+//         effectLevelDepth.style.width = effectLevelPin.style.left;
+//       }
+//     }
+//   };
+//
+//   var onMouseUp = function (upEvt) {
+//     upEvt.preventDefault();
+//     document.removeEventListener('mousemove', onMouseMove);
+//     document.removeEventListener('mouseup', onMouseUp);
+//     if (dragged) {
+//       var onClickPreventDefault = function (clickEvt) {
+//         clickEvt.preventDefault();
+//         effectLevelPin.removeEventListener('click', onClickPreventDefault);
+//       };
+//       effectLevelPin.addEventListener('click', onClickPreventDefault);
+//     }
+//   };
+//
+//   document.addEventListener('mousemove', onMouseMove);
+//   document.addEventListener('mouseup', onMouseUp);
+// });
