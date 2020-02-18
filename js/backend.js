52b626ea98933dddb338acc3ffb525d9961196ec
Kaputin <kaputinevgeny@gmail.com>
2020-02-18 03:42:37 +0300

    Внес исправления согласно комментариям, сделал следующее задание на получения данных

---
+++ js/backend.js
@@ -0,0 +1,33 @@
+'use strict';
+(function () {
+  var URL_LOAD = 'https://js.dump.academy/kekstagram/data';
+  var STATUS_CODE = {
+    OK: 200
+  };
+  var TIMEOUT_IN_MS = 10000;// 10s
+
+  window.backendLoad = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === STATUS_CODE.OK) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = TIMEOUT_IN_MS;
+
+    xhr.open('GET', URL_LOAD);
+    xhr.send();
+  };
+
+})();
